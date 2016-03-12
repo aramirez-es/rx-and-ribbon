c31af2a05d548fc41f39464ab7ac2a26a24856e3
Alberto <alberto@aramirez.es>
2016-03-12 18:57:46 +0100

    Controller now gets queryString

--- src/main/java/es/aramirez/rxribbon/Controller.java
+++ src/main/java/es/aramirez/rxribbon/Controller.java
@@ -10,6 +10,8 @@ import io.reactivex.netty.protocol.http.server.RequestHandler;
 import rx.Observable;
 
 import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
 
 public class Controller implements RequestHandler<ByteBuf, ByteBuf> {
   private final Service service;
@@ -28,10 +30,19 @@ public class Controller implements RequestHandler<ByteBuf, ByteBuf> {
     HttpServerResponse<ByteBuf> response
   ) {
     return request.getContent()
-      .flatMap(byteBuf -> Observable.just(byteBuf.toString(Charset.defaultCharset())))
-      .flatMap(requestAsString -> service.execute(new ServiceRequest("Person", 42.2, 15.5, 42)))
+      .map(byteBuf -> toServiceRequest(request))
+      .flatMap(service::execute)
       .flatMap(aggregated -> setResponse(response, aggregated))
       .finallyDo(response::close);
+  }
+
+  private ServiceRequest toServiceRequest(HttpServerRequest<ByteBuf> request) {
+    Map<String, List<String>> queryString = request.getQueryParameters();
+    return new ServiceRequest(
+      queryString.get("user").get(0),
+      Double.valueOf(queryString.get("latitude").get(0)),
+      Double.valueOf(queryString.get("longitude").get(0)),
+      Integer.valueOf(queryString.get("itemId").get(0)));
   }
 
   private Observable<Void> setResponse(
